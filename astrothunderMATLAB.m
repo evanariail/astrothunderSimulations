b4abc02a163441026f06bf2b1f7ca4a0e67ac4ad
Evan Ariail <evanariail26@gmail.com>
2024-09-27 19:07:25 -0400

    added constants

--- astrothunderMATLAB.m
+++ astrothunderMATLAB.m
@@ -1,7 +1,19 @@
-function[out] = astrothunderMATLAB(in)
-    function[out2] = test(in2)
-        out2 = in2 + 4;
-        end
-    out2 = test(4);
-    out = in + 3 + out2;
-    end
+function astrothunderMATLAB
+
+%Constants
+charVel = 5020; %ft/s
+burnRateCoef = 0.0375;
+burnRateExp = 0.3;
+propDensity = 0.0018; %slugs/in^3
+stagTemp = 4700; %R
+propSpecGasConstant = 200; %psi*ft^3/slugs*R
+ratioSpecHeats = 1.2;
+g = 32.2; %ft/s^2
+ambientPressure = 14.7; %psi
+
+deltat = 0.001; %s
+
+
+
+
+end
